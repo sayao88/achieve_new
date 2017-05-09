aa23813e6571bb09458e18cd0af9814522054fea
so <pps6777@gmail.com>
2017-05-09 02:13:03 +0000

    modify spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe Contact do
-  # タイトルがあれば有効な状態であること
-    it "is valid with title,email,content" do
+  #入力内容があれば有効な状態であること
+    it "is valid with name,email,content" do
         contact = Contact.new(name:'yamada',email:'yamada@mail.com',content:'教えてください')
         expect(contact).to be_valid
     end
-  #タイトルがなければ無効であること
-    it "is invalid without title,email,content" do
+  #入力内容がなければ無効であること
+    it "is invalid without name,email,content" do
         contact = Contact.new
         expect(contact).not_to be_valid
     end
